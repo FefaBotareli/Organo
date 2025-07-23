c9b0dd56692a0c8a5370af1aa5d21c7988ae048f
Fernanda Botareli <febotareli@gmail.com>
2025-07-22 22:26:17 -0300

    Adicionado componentes no  Botao e ListaSuspensa e atualizados os indexes de  Formulario e CampoTexto

--- src/components/componentes/Formulario/index.js
+++ src/components/componentes/Formulario/index.js
@@ -1,18 +1,34 @@
+import Botao from "../Botao";
 import CampoTexto from "../CampoTexto";
+import ListaSuspensa from "../ListaSuspensa";
 import "./Formulario.css";
 
 const Formulario = () => {
+
+  const generos = [
+    'Romance',
+    'Fantasia',
+    'Mistério',
+    'Suspense',
+    'Clássico'
+  ]
+
+  const aoSalvar = (evento) =>{
+    evento.preventDefault()
+    console.log('Form foi submetido')
+  }
+
   return (
     <section className="formulario">
-      <form>
+      <form onSubmit={aoSalvar}>
         <h2>Preencha os dados para criar o card do(a) autor(a)</h2>
-        <CampoTexto label="Nome" placeholder="Digite o nome do(a) autor(a)" />
-        <CampoTexto label="Livro" placeholder="Digite o livro mais popular" />
+        <CampoTexto obrigatorio={true} label="Nome" placeholder="Digite o nome do(a) autor(a)" />
+        <CampoTexto obrigatorio={true} label="Livro" placeholder="Digite o livro mais popular" />
         <CampoTexto label="Imagem" placeholder="Digite o endereço de imagem" />
-        <CampoTexto
-          label="Gênero"
-          placeholder="Selecione o gênero principal do autor"
-        />
+        <ListaSuspensa obrigatorio={true} label="Gênero" itens={generos}/>
+        <Botao> 
+            Criar Card
+          </Botao>
       </form>
     </section>
   );
